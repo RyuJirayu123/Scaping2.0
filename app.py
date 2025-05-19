3ff719b48c8738ac51463a74c5e9e5cff06ebedf
RyuJirayu123 <rew254967@gmail.com>
2025-05-19 17:25:44 +0700

    ok

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from io import BytesIO
 
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 GOOGLE_CSE_ID = st.secrets["GOOGLE_CSE_ID"]
-
 # ---------------------------
 # Sidebar: Input
 # ---------------------------
